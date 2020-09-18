c5f2a61cb86e9aca5e128c6f7e574a1debd28a6d
parth-5097 <parth5097@gmail.com>
2020-09-18 21:56:52 +0530

    7: just developing middleware services

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <div>Just Testing Platform: Live Updating with something</div>,
+    document.getElementById('root')
+);

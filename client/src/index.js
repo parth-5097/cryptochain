25ba5274e059adf8ed14f1e53fce62cd1fedaad0
parth-5097 <parth5097@gmail.com>
2020-09-19 20:24:44 +0530

    11: 98% frontend complete

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,17 @@ import { Router, Switch, Route } from 'react-router-dom';
 import history from './history';
 import Blocks from './components/Blocks';
 import App from './components/App';
+import ConductTransaction from './components/ConductTransaction';
+import TransactionPool from './components/TransactionPool';
 import './index.css';
 
 render(
     <Router history={history}>
         <Switch>
-            <Route exact path='/' component={App} />
+            <Route exact={true} path='/' component={App} />
             <Route path='/blocks' component={Blocks} />
+            <Route path='/conduct-transaction' component={ConductTransaction} />
+            <Route path='/transaction-pool' component={TransactionPool} />
         </Switch>
     </Router>,
     document.getElementById('root')

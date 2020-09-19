9f5fd67eb06840788bb1c429969b0b8fc4091f71
parth-5097 <parth5097@gmail.com>
2020-09-19 13:19:41 +0530

    10: frontend cryptocurrency

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Block from './Block';
 
 class Blocks extends Component {
@@ -15,11 +16,12 @@ class Blocks extends Component {
 
         return (
             <div>
+                <div><Link to='/'>Home</Link></div>
                 <h3>Blocks</h3>
                 {
                     this.state.blocks.map(block => {
                         return (
-                            <Block key={block.hash} block={block}/>
+                            <Block key={block.hash} block={block} />
                         );
                     })
                 }
